7e1b4a45d8e32b1595d2dfb0928bf63404be345a
startdusk <522383345@qq.com>
2022-07-02 21:39:16 +0800

    feat✨:  init

---
+++ Makefile
@@ -0,0 +1,10 @@
+CMD=go
+
+# Makfile using condtional: https://web.mit.edu/gnu/doc/html/make_7.html#:~:text=A%20conditional%20causes%20part%20of,variable%20to%20a%20constant%20string.
+run:
+ifeq ($(bin), front-end)
+	$(CMD) run ./$(bin)/cmd/web/main.go
+endif
+ifeq ($(bin), broker-service)
+	$(CMD) run ./$(bin)/cmd/api/main.go
+endif
---
+++ broker-service/cmd/api/handlers.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Hit the broker",
+	}
+
+	out, err := json.MarshalIndent(payload, "", "\t")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write(out)
+}
